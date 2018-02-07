a996696d96f31839d6bcb8d294a7cef3794a91cf
Diveyez <nemofbaltimore@gmail.com>
2018-02-07 07:11:53 +0000

    Update mach.sh

--- mach.sh
+++ mach.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 # MACH - ifconfig based mac changer
-echo "Network Manager? If you dont have it uncomment!"
-service network-manager stop
 echo "You may need to change the names of your interfaces!"
-ifconfig eth0 down && ifconfig wlan0 down && ifconfig wlan1 down
-ifconfig eth0 hw ether 00:00:00:00:00:00
-ifconfig wifi0 hw ether 00:00:00:00:00:00
-ifconfig wifi1 hw ether 00:00:00:00:00:00
-service network-manager start
-ifconfig eth0 up && ifconfig wlan0 up && ifconfig wlan1 up
-# more functions to come
+sleep 5.0
+#ifconfig eth0 down
+#ifconfig wlan0 down
+#ifconfig wlan1 down
+/etc/init.d/networking stop
+sleep 0.1
+ifconfig eth0 down
+ifconfig eth0 hw ether 00:00:00:00:00:01
+sleep 0.3
+ifconfig eth0 up
+ifconfig wlan0 down
+ifconfig wlan0 hw ether 00:00:00:00:00:05
+sleep 0.3
+ifconfig wlan0 up
+ifconfig wlan1 down
+ifconfig wlan1 hw ether 00:00:00:00:00:07
+sleep 0.3
+ifconfig wlan1 up
+sleep 0.1
+/etc/init.d/networking start
+# UMMM, FIX THIS
 echo "Finished! Your interfaces now have a 00:00:00:00:00:00 MAC Address!"
