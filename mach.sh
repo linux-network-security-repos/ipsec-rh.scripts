8aefe5d950ffa756988eb5975746d891ffe14011
Diveyez <nemofbaltimore@gmail.com>
2018-02-10 21:22:06 -0500

    Now at MACH 2.0
    Since maccchanger -r didnt want to work, I made this. Will implement in other scripts. =)

--- mach.sh
+++ mach.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
-# MACH - ifconfig based mac changer
-echo "You may need to change the names of your interfaces!"
-sleep 5.0
+# MACH - ifconfig based mac changer written by Diveyez 2/10/2018
+echo "You may need to change the names of your interfaces in this script for it to work!"
+sleep 3.0
 #ifconfig eth0 down
 #ifconfig wlan0 down
 #ifconfig wlan1 down
-/etc/init.d/networking stop
+#/etc/init.d/networking stop
 sleep 0.1
 ifconfig eth0 down
-ifconfig eth0 hw ether 00:00:00:00:00:01
+ifconfig eth0 hw ether $(printf '02:%02X:%02X:%02X:%02X:%02X \n' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
 sleep 0.3
 ifconfig eth0 up
+sleep 0.1
 ifconfig wlan0 down
-ifconfig wlan0 hw ether 00:00:00:00:00:05
+ifconfig wlan0 hw ether $(printf '02:%02X:%02X:%02X:%02X:%02X \n' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
 sleep 0.3
 ifconfig wlan0 up
+sleep 0.1
 ifconfig wlan1 down
-ifconfig wlan1 hw ether 00:00:00:00:00:07
+ifconfig wlan1 hw ether $(printf '02:%02X:%02X:%02X:%02X:%02X \n' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
 sleep 0.3
 ifconfig wlan1 up
+sleep 0.3
+ifconfig wlan2 down
+ifconfig wlan2 hw ether $(printf '02:%02X:%02X:%02X:%02X:%02X \n' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
+sleep 0.3
+ifconfig wlan2 up
 sleep 0.1
-/etc/init.d/networking start
+#/etc/init.d/networking restart
 # UMMM, FIX THIS
-echo "Finished! Your interfaces now have a 00:00:00:00:00:0X MAC Address!"
+echo "Finished! Your interfaces now have a entirely random MAC Address!"
 # Some Reason It Broke a Realtek Card. Be Careful...
 # YIKES
+
