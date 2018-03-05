f74a9d5119365655b3d097d9b98ea63f40542961
Diveyez <nemofbaltimore@gmail.com>
2018-03-05 04:44:53 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo "COMING SOON!"
+echo "For now we just start with changing mac's"
+ifconfig wlan1mon down && ifconfig wlan2mon down
+macchanger -r wlan1mon && macchanger -r wlan2mon
+echo "I hope those were your antennas"
